1c14c35caf972d22c857e80b621fb75c1732e85a
Yari Andres <yari.andres@gmail.com>
2019-08-16 10:39:33 +0200

    displaying JSON data to the page

--- amazonSearchTermData/js/app.js
+++ amazonSearchTermData/js/app.js
@@ -1,9 +1,37 @@
-document.title = "Amazon Ads search term Analyser";
+document.title = "Amazon Bulk file Keyword Editor";
 
-window.onload = async () => {
-  const response = await fetch('../data/data.json');
-  const stats = await response.json();
+var Http = new XMLHttpRequest();
 
-  console.log(stats.features[0].properties)
+Http.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+
+    // parsing the response to a javascript object
+    var response = JSON.parse(Http.responseText);
+
+    console.log(response[0].Campaign)
+
+    var output = '';
+    for (var i = 0; i < response.length; i++) {
+      // console.log(response[i].Campaign)
+      output += `
+        <tr>
+          <th scope="row">${response[i].Campaign}</th>
+          <td>${response[i].Spend}</td>
+          <td>${response[i].Clicks}</td>
+          <td>${response[i].ACoS}</td>
+        </tr>
+      `;
+    }
+    // getting the table body for displaying the campaign name
+    document.querySelector('#tableColumn').innerHTML = output;
+
+
+
+
+    // console.log(response)
+  }
 
 }
+
+Http.open("GET", "data/bulk-template.json", true);
+Http.send()
