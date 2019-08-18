c66ab57e25dfe45a1e308f1610f92a614c4ca29d
Yari Andres <yari.andres@gmail.com>
2019-08-18 23:58:47 +0200

    adding fix bid functionality

--- amazonSearchTermData/js/app.js
+++ amazonSearchTermData/js/app.js
@@ -7,31 +7,76 @@ Http.onreadystatechange = function () {
 
     // parsing the response to a javascript object
     var response = JSON.parse(Http.responseText);
-
-    console.log(response[0].Campaign)
-
+    // console.log(response[0].Campaign)
     var output = '';
     for (var i = 0; i < response.length; i++) {
       // console.log(response[i].Campaign)
       output += `
         <tr>
           <th scope="row">${response[i].Campaign}</th>
-          <td>${response[i].Spend}</td>
-          <td>${response[i].Clicks}</td>
-          <td>${response[i].ACoS}</td>
+          <td>${response[i].KeywordorProductTargeting}</td>
+          <td id="spend">${response[i].Spend}</td>
+          <td id="clicks">${response[i].Clicks}</td>          
+          <td>
+            <input 
+              type="number" 
+              class="form-control" 
+              id="targetACoS" 
+              placeholder="Target">
+          </td>
+          <td id="currentAcos">${response[i].ACoS}</td>
+          <td>
+            <button 
+              type="submit" 
+              class="btn btn-info"
+              onclick="calculate()">
+                Fix
+            </button>
+          </td>
+          <td>1.09</td>          
         </tr>
       `;
     }
+
     // getting the table body for displaying the campaign name
     document.querySelector('#tableColumn').innerHTML = output;
-
-
-
-
-    // console.log(response)
   }
 
 }
-
 Http.open("GET", "data/bulk-template.json", true);
-Http.send()
+Http.send();
+
+calculate = () => {
+  // getting spen value
+  spendValue = document.querySelector('#spend').innerText;
+  // Converting Strings to Numbers
+
+  // Converting Strings to Numbers
+  valueAsNum = parseFloat(spendValue);
+  console.log(valueAsNum);
+
+  // getting the number of clicks
+  numOfClicks = document.querySelector('#clicks').innerText;
+  // Converting Strings to Numbers
+  clickAsNum = parseFloat(numOfClicks);
+  console.log(clickAsNum);
+
+  // getting user input 
+  retriveText = () => {
+
+    // getting the input field
+    var userInput = document.querySelector('#targetACoS').value;
+    // Converting Strings to Numbers
+    inputAsNum = parseFloat(userInput);
+    console.log(inputAsNum)
+
+    if (inputAsNum === NaN) {
+      return elert("enter a number")
+    } else {
+      return console.log("condition passed")
+    }
+
+
+  }
+  retriveText()
+}
