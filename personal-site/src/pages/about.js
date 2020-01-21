1e1b0df646e465afaed7b5ca012bddd62e8b9148
Yari Andres <yari.andres@gmail.com>
2020-01-21 18:47:51 +0100

    added markdown file with test blog data & added link to navigation at the front

--- personal-site/src/pages/about.js
+++ personal-site/src/pages/about.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import Layout from "../components/layout"
-import Menu from "../components/menu"
-import SEO from "../components/seo"
+// import Menu from "../components/menu"
+// import SEO from "../components/seo"
 
 const about = () => 
  (
