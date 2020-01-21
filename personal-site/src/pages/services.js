2a378c0f5bbcf9b761634427cd8f6a69a2f6674b
Yari Andres <yari.andres@gmail.com>
2020-01-21 17:59:05 +0100

    adding personal site starter with gatsby

---
+++ personal-site/src/pages/services.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Layout from "../components/layout"
+import Menu from "../components/menu"
+import SEO from "../components/seo"
+
+const services = () => 
+  (
+    <Layout>
+      <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugiat blanditiis quae laborum, possimus praesentium, consequuntur doloremque accusamus autem, officiis architecto quaerat repudiandae quo dolorem optio harum soluta quasi ratione!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugiat blanditiis quae laborum, possimus praesentium, consequuntur doloremque accusamus autem, officiis architecto quaerat repudiandae quo dolorem optio harum soluta quasi ratione!</p>
+      </div>
+    </Layout>  
+  );
+
+
+export default services;
