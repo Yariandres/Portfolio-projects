48803fb00c2d19e32c3b7d2a829746b30c64d75b
Yari Andres <yari.andres@gmail.com>
2020-03-20 21:55:28 +0100

    aading handle bid fuction

--- amazonSearchTermData/js/app.js
+++ amazonSearchTermData/js/app.js
@@ -1,82 +1,93 @@
-document.title = "Amazon Bulk file Keyword Editor";
+let rowData = [];
 
-var Http = new XMLHttpRequest();
-
-Http.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-
-    // parsing the response to a javascript object
-    var response = JSON.parse(Http.responseText);
-    // console.log(response[0].Campaign)
-    var output = '';
-    for (var i = 0; i < response.length; i++) {
-      // console.log(response[i].Campaign)
-      output += `
-        <tr>
-          <th scope="row">${response[i].Campaign}</th>
-          <td>${response[i].KeywordorProductTargeting}</td>
-          <td id="spend">${response[i].Spend}</td>
-          <td id="clicks">${response[i].Clicks}</td>          
-          <td>
-            <input 
-              type="number" 
-              class="form-control" 
-              id="targetACoS" 
-              placeholder="Target">
-          </td>
-          <td id="currentAcos">${response[i].ACoS}</td>
-          <td>
-            <button 
-              type="submit" 
-              class="btn btn-info"
-              onclick="calculate()">
-                Fix
-            </button>
-          </td>
-          <td>1.09</td>          
-        </tr>
-      `;
-    }
-
-    // getting the table body for displaying the campaign name
-    document.querySelector('#tableColumn').innerHTML = output;
-  }
-
+window.onload = async () => {
+  let response = await fetch('../data/bulk-template.json');
+  rowData = await response.json();
+  createDataList(rowData)
 }
-Http.open("GET", "data/bulk-template.json", true);
-Http.send();
-
-calculate = () => {
-  // getting spen value
-  spendValue = document.querySelector('#spend').innerText;
-  // Converting Strings to Numbers
-
-  // Converting Strings to Numbers
-  valueAsNum = parseFloat(spendValue);
-  console.log(valueAsNum);
-
-  // getting the number of clicks
-  numOfClicks = document.querySelector('#clicks').innerText;
-  // Converting Strings to Numbers
-  clickAsNum = parseFloat(numOfClicks);
-  console.log(clickAsNum);
-
-  // getting user input 
-  retriveText = () => {
-
-    // getting the input field
-    var userInput = document.querySelector('#targetACoS').value;
-    // Converting Strings to Numbers
-    inputAsNum = parseFloat(userInput);
-    console.log(inputAsNum)
-
-    if (inputAsNum === NaN) {
-      return elert("enter a number")
-    } else {
-      return console.log("condition passed")
-    }
 
 
-  }
-  retriveText()
+handleBidCalculation = () => {   
+  let spend = document.querySelector('#spend').innerHTML;
+  let clicks = document.querySelector('#clicks').innerHTML;
+  let target = document.querySelector('#targetACoS').innerHTML;
+  let acos = document.querySelector('#currentAcos').innerHTML;
+  let result = spend / clicks * target / parseFloat(acos);
+  
+  document.querySelector("#bidResult").innerHTML = result.toFixed(2);
 }
+
+
+createDataList =  rowList => {
+  console.log(rowData);
+  const html = rowList.map(rowData => `
+    <tr>
+      <th scope="row">${rowData.Campaign}</th>
+      <td>${rowData.KeywordorProductTargeting}</td>
+      <td id="spend">${rowData.Spend}</td>
+      <td id="clicks">${rowData.Clicks}</td>
+      <td id="targetACoS">25</td>
+      <td id="currentAcos">${rowData.ACoS}</td>
+      <td>
+        <p id="btnAction" class="btn btn-info" onclick="handleBidCalculation()">
+          Fix
+        </p>
+      </td>
+      <td id="bidResult"></td>
+    </tr>
+    `);
+
+    document.querySelector('#tableColumn').innerHTML = html;
+}
+
+
+
+  // let spendValue, clicksValue, targetValue, acosValue;
+  // spendValue = parseFloat(spend);
+  // clicksValue = parseFloat(clicks);
+  // targetValue = parseFloat(target);
+  // acosValue = parseFloat(acos);  
+  // let result = spendValue / clicksValue * targetValue / acosValue;
+
+
+
+// Http.open("GET", "data/bulk-template.json", true);
+// Http.send();
+
+// calculate = () => {
+
+//  preventDefault();
+
+//   // getting spen value
+//   spendValue = document.querySelector('#spend').innerText;
+
+//   // Converting Strings to Numbers
+//   valueAsNum = parseFloat(spendValue);
+//   console.log(valueAsNum);
+
+//   // getting the number of clicks
+//   numOfClicks = document.querySelector('#clicks').innerText;
+
+//   // Converting Strings to Numbers
+//   clickAsNum = parseFloat(numOfClicks);
+
+//   console.log(clickAsNum);
+
+//   // getting user input 
+//   retriveText = () => {
+
+//     // getting the input field
+//     var userInput = document.querySelector('#targetACoS').value;
+//     // Converting Strings to Numbers
+//     inputAsNum = parseFloat(userInput);
+//     console.log(inputAsNum)
+
+//     if (inputAsNum === NaN) {
+//       return elert("enter a number")
+//     } else {
+//       return console.log("condition passed")
+//     }
+
+//   }
+//   retriveText()
+// }
